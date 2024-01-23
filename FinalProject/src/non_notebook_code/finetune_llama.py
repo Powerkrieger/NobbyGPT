98433960242ba10b5d0c3128272630eecbdee5ac
powerkrieger <power.krieger@gmx.de>
2024-01-23 16:29:05 +0000

    evaluation

--- FinalProject/src/non_notebook_code/finetune_llama.py
+++ FinalProject/src/non_notebook_code/finetune_llama.py
@@ -13,9 +13,9 @@ from datetime import datetime
 
 base_model_name = "Llama-2-7b-hf"
 base_model_id = f"meta-llama/{base_model_name}"
-train_data_file = '../../Data/Training_Data/WeeveIE_Wikipedia/WeeveLVL3_J.json'
-eval_data_file = '../../Data/Training_Data/WeeveIE_Wikipedia/WeeveLVL3_J.json'
-project_name = "denglish-weeve_lvl3"
+train_data_file = '../../Data/Training_Data/WeeveIE_Wikipedia/WeeveLVL4_J.json'
+eval_data_file = '../../Data/Training_Data/WeeveIE_Wikipedia/WeeveLVL4_J.json'
+project_name = "denglish-weeve_lvl4"
 
 
 def formatting_func(example):

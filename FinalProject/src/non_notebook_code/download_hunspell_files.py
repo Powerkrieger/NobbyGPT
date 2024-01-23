379a5fd2a66a82dc6e3b25411320b756d99435c5
jorehder <joost.rehder@igd-r.fraunhofer.de>
2024-01-23 13:41:33 +0100

    script to download hunspell needed files

---
+++ FinalProject/src/non_notebook_code/download_hunspell_files.py
@@ -0,0 +1,7 @@
+import urllib
+
+if __name__ == "__main__":
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/elastic/hunspell/master/dicts/de_DE/de_DE.dic",
+                               "de_DE.dic")
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/elastic/hunspell/master/dicts/de_DE/de_DE.aff",
+                               "de_DE.aff")

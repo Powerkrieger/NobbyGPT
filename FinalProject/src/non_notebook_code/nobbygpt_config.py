19c4c4560324d4053e527cd525eda917ef1bd36b
powerkrieger <power.krieger@gmx.de>
2024-01-29 18:58:09 +0000

    running textbook example

--- FinalProject/src/non_notebook_code/nobbygpt_config.py
+++ FinalProject/src/non_notebook_code/nobbygpt_config.py
@@ -1,5 +1,5 @@
 base_model_name = "Llama-2-7b-hf"
 base_model_id = f"meta-llama/{base_model_name}"
-train_data_file = '../../Data/Training_Data/WeeveIE_Wikipedia/WeeveLVL3_J.json'
-eval_data_file = '../../Data/Training_Data/WeeveIE_Wikipedia/WeeveLVL3_J.json'
-project_name = "denglish-weeve_lvl3"
+train_data_file = '../../Data/Training_Data/ger_book.json'
+eval_data_file = '../../Data/Training_Data/ger_book.json'
+project_name = "textbook-german"
--- FinalProject/src/non_notebook_code/start.sh
+++ FinalProject/src/non_notebook_code/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#python ./finetune_llama.py;
-python ./inference_llama.py "denglish-weeve_lvl2" >../../Data/Generated_Samples/inference_lvl2/sample002.txt;
-python ./inference_llama.py "denglish-weeve_lvl3" >../../Data/Generated_Samples/inference_lvl3/sample002.txt;
-python ./inference_llama.py "denglish-weeve_lvl4" >../../Data/Generated_Samples/inference_lvl4/sample002.txt;
+python ./finetune_llama.py;
+#python ./inference_llama.py "denglish-weeve_lvl2" >../../Data/Generated_Samples/inference_lvl2/sample002.txt;
+#python ./inference_llama.py "denglish-weeve_lvl3" >../../Data/Generated_Samples/inference_lvl3/sample002.txt;
+#python ./inference_llama.py "denglish-weeve_lvl4" >../../Data/Generated_Samples/inference_lvl4/sample002.txt;
+python ./inference_llama.py "textbook-german" >../../Data/Generated_Samples/textbook-german/sample002.txt;
